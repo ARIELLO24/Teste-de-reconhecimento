bf3579bd92d3189354f9bf22485b45f01d7a6a5c
ARIELLO24 <162522631+ARIELLO24@users.noreply.github.com>
2024-05-23 22:00:12 -0300

    tipos de dados.js

--- tipos de dados.js
+++ tipos de dados.js
@@ -8,4 +8,4 @@ const p = { // Objeto
   firstName: "John", // String
   lastName1: "Doe" // String
 };
-console.log("Olá, meu nome é", p.firstName, "e meu sobrenome é", p.lastName1, "e minha idade é", length);
+console.log("Olá, meu nome é", p.firstName, "e meu sobrenome é", p.lastName1, "e minha idade é", length)
