8585692d5450ed6cb1eb2bd2e79b2288a154a2d9
ARIELLO24 <162522631+ARIELLO24@users.noreply.github.com>
2024-05-23 22:00:55 -0300

    operadores.js

--- operadores.js
+++ operadores.js
@@ -15,10 +15,10 @@ console.log(resto)
 
 //Use o operador de atribuição correto para fazer x ser igual a 15 (o mesmo que escrever x = x + y).
 
-x = 10;
-y = 5;
-x += y; 
-console.log(x); 
+x = 10
+y = 5
+x += y
+console.log(x)
 
 
 
