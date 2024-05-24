e3285d15c8885b4523d8989c324869958f1e3964
ARIELLO24 <162522631+ARIELLO24@users.noreply.github.com>
2024-05-23 21:59:39 -0300

    Loop while.js

--- Loop while.js
+++ Loop while.js
@@ -3,7 +3,7 @@
 //Crie um loop que continua executando enquanto i for menor que 10.
 let i = 0;
 while (i < 10) {
-  console.log(i);
+  console.log(i)
   i++;
 }
 
@@ -11,6 +11,6 @@ while (i < 10) {
 //Crie um loop que continua executando enquanto i for menor que 10, mas incremente i com 2 a cada vez.
 let r = 0;
 while (r < 10) {
-  console.log(i);
-  r += 2;
+  console.log(i)
+  r += 2
 }
