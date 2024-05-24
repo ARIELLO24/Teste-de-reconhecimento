7a243edc57f71f8487e8434c5809f1c924743449
ARIELLO24 <162522631+ARIELLO24@users.noreply.github.com>
2024-05-23 22:03:36 -0300

    Loop for.js

--- Loop for.js
+++ Loop for.js
@@ -2,12 +2,12 @@
 
 // Loop de contagem de 0 a 9
 for (let i = 0; i < 10; i++) {
-    console.log(i);
+    console.log(i)
   }
   
   // Loop para percorrer todos os itens do array "fruits"
   const fruits = ["Apple", "Banana", "Orange"]
   for (let i = 0; i < fruits.length; i++) {
-    console.log(fruits[i]);
+    console.log(fruits[i])
   }
   
